ebdd3e591d258a5ec8631f2cbecc9473114f51cd
helloworld556178 <helloworld556178@outlook.com>
2021-09-09 08:21:52 +0800

    添加了一条新闻

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
+# 记录
 1. js函数的调用者的确定方式（可能） `o.foo` ，没有点的就是 `globalThis.foo`
 2. 类的实例拥有一个指向原型的指针而不是副本，修改了类的 `prototype`，那么所有的实例的 `prototype` 指向的属性都会改变
 3. `JSON.stringify`会忽略 `undefined`，不会忽略 `null`，`NaN`，`Infinity`等无效数据会被转化为 `null`
 4. js的自增自减运算符，如果写在变量后面，执行方式应该是`返回,变量修改,执行后面的计算`，这个修改大概是立即的，即 `var a=1; a++ + a === 3;`
 5. `console.log(Object.is(NaN, NaN), true, '*')`
+
+# 新闻
+## 阿里女员工被侵害案
+> 2021年09月07日文
+1. [判定为猥亵，不入刑](https://mbd.baidu.com/newspage/data/landingsuper?context=%7B%22nid%22%3A%22news_8665461110681100718%22%7D&n_type=-1&p_from=-1)
+2. 周某状告张某文性侵
+3. 结果：判定为猥亵，性侵行为不成立，状告失败
--- src/util/declare.ts
+++ src/util/declare.ts
@@ -1,1 +1,40 @@
-export type SimpleFunction = (v?: any) => void;
+//#region 通用功能
+export type SimpleFunction = (value?: any) => void;
+export type SimpleBooleanFunction = (value?: any) => boolean;
+//#endregion
+
+//#region 类型对象声明
+export interface ZTree {
+    refresh(): void;
+    getNodesByFilter(fn: (node: ZTreeNode) => boolean, isSingle: true, parentNode?: ZTreeNode): ZTreeNode;
+    getNodesByFilter(fn: (node: ZTreeNode) => boolean, isSingle?: false, parentNode?: ZTreeNode): ZTreeNode[];
+    getNodesByParam(key: string, value: any, parentNode?: ZTreeNode): ZTreeNode[];
+    checkAllNodes(checked: boolean): void;
+    checkNode(node: ZTreeNode, checked?: boolean, checkTypeFlag?: boolean, callbackFlag?: boolean): void;
+    showNodes(nodes: ZTreeNode[]): void;
+    hideNodes(nodes: ZTreeNode[]): void;
+    transformToArray(nodes: ZTreeNode[]): ZTreeNode[];
+    getNodes(): ZTreeNode[];
+    destroy(): void;
+    cancelSelectedNode(node?: ZTreeNode): void;
+    expandAll(expandFlag: boolean): boolean;
+
+    setting: {
+        callback: {
+            onClick: (ev?: Event, treeId?: string, treeNode?: ZTreeNode, clickFlag?: 0 | 1 | 2) => void;
+        };
+    }
+}
+export interface ZTreeNode {
+    checked: boolean;
+    isParent: boolean;
+    isHidden: boolean;
+    name: string;
+
+    readonly level: number;
+    children: ZTreeNode[];
+    getParentNode(): ZTreeNode;
+
+    [key: string]: any;
+}
+//#endregion
