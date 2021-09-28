6c4a4bbb63f2eb6b33cc32d3b6064c1cb86dc9e9
helloworld556178 <helloworld556178@outlook.com>
2021-09-28 17:58:42 +0800

    添加 .d.ts

---
+++ src/lib/sql-wasm.d.ts
@@ -0,0 +1,5 @@
+/**
+ * 这是自己为 sql-wasm 写的，对一些可能用到的方法添加了定义
+ */
+
+export * as SQL from "./sql-wasm.detail";
---
+++ src/lib/sql-wasm.detail.d.ts
@@ -0,0 +1,7 @@
+
+export declare class Database {
+    public run(sql: string): void;
+    public prepare(pattern: string): STMT;
+}
+
+export declare class STMT { }
