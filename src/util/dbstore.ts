c80affbe75eabe36d6fc48d80a76d1efe7ffe86d
helloworld556178 <helloworld556178@outlook.com>
2022-03-30 09:44:27 +0800

    使用indexeddb的简单nosql封装

---
+++ src/util/dbstore.ts
@@ -0,0 +1,80 @@
+import { SimplePromise } from "./promise";
+
+export class DBStore {
+    private static readonly OBJECT_NAME = "STORE";
+    public readonly name: string;
+    public readonly version: number;
+    public get ready(): boolean { return this.promise.finished; }
+
+    private db: IDBDatabase;
+    private promise: SimplePromise<void>;
+
+    constructor(name: string, version: number) {
+        this.name = name;
+        this.version = version;
+        this.promise = new SimplePromise<void>();
+        this.init();
+    }
+
+    private init(): void {
+        const indexedDB: IDBFactory = window.indexedDB || window["mozIndexedDB"] || window["webkitIndexedDB"] || window["msIndexedDB"];
+        if (!indexedDB) { return; }
+        const request = indexedDB.open(this.name, this.version);
+        request.onerror = (event: Event) => {
+            this.promise.reject(event);
+        };
+        request.onsuccess = (event: Event) => {
+            this.db = event.target["result"];
+            this.promise.resolve();
+        };
+        request.onupgradeneeded = (event: Event) => {
+            const db: IDBDatabase = event.target["result"];
+            if (!db.objectStoreNames.contains(DBStore.OBJECT_NAME)) {
+                const store = db.createObjectStore(DBStore.OBJECT_NAME, { keyPath: "key" });
+                store.createIndex("id", "id", { unique: true });
+            }
+        };
+    }
+
+    private async store(): Promise<IDBObjectStore> {
+        await this.promise.promise;
+        return this.db.transaction(DBStore.OBJECT_NAME, "readwrite").objectStore(DBStore.OBJECT_NAME);
+    }
+
+    public async delete(key: string): Promise<void> {
+        const store = await this.store();
+        const promise = new SimplePromise();
+        const request = store.delete(key);
+        request.onsuccess = () => promise.resolve();
+        request.onerror = (event: Event) => promise.reject(event);
+        return promise.promise;
+    }
+    public async set(key: string, value: any): Promise<void> {
+        const store = await this.store();
+        const promise = new SimplePromise();
+        const request = store.put({ "id": key, value: value }, key);
+        request.onsuccess = () => promise.resolve();
+        request.onerror = (event: Event) => promise.reject(event);
+        return promise.promise;
+    }
+    public async get(key: string): Promise<any> {
+        const store = await this.store();
+        const index = store.index("id");
+        const request = index.get(key);
+        const promise = new SimplePromise();
+        request.onsuccess = (event: Event) => promise.resolve(event.target["result"]["value"]);
+        request.onerror = (event: Event) => promise.reject(event);
+        return promise.promise;
+    }
+    public async then(callback: () => Promise<void> | void): Promise<void> {
+        await this.promise.promise;
+        if (!this.db.objectStoreNames.contains(DBStore.OBJECT_NAME)) {
+            return Promise.reject("does not have store, please upgrade database version");
+        }
+        return callback();
+    }
+    // 没什么意义
+    public catch(callback: (error: Event) => void): Promise<void> {
+        return this.promise.promise.catch(callback);
+    }
+}
